57869fc93c2735fb68aba5d0986b10aa3e4ca7da
thecyclingfarmer <martgust@gmail.com>
2015-08-21 12:33:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Seed project
+
+A seed project..... TBD
+
+```bash
+npm install
+gulp develop
+```
+
+Open another terminal to run tests
+
+```bash
+gulp test
+```
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require("gulp");
+var shell = require('gulp-shell');
+var plugins = require('gulp-load-plugins')({scope: ['dependencies']});
+var sourcemaps = require("gulp-sourcemaps");
+var runSequence = require('run-sequence');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var del = require('del');
+
+gulp.task('clean', function (cb) {
+  del('components', function () {
+    cb();
+  });
+});
+
+gulp.task('mocha', shell.task('npm test', {
+    // So our task doesn't error out when a test fails
+    ignoreErrors: true
+}));
+
+gulp.task('test', function () {
+  runSequence('mocha');
+  gulp.watch(['js/**/*.js','test/**/*.js'], ['mocha'])
+});
+
+gulp.task('build', function () {
+    browserify({
+    	entries: 'js/app.js',
+    	debug: true
+    })
+    .transform(babelify.configure({
+		  stage: 0 //Use ES7 transforms
+    }))
+    .bundle()
+    .pipe(source('./js/app.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+
+
+gulp.task('develop', function (cb) {
+    runSequence(
+        'clean',
+        'build',
+        'serve',
+        cb
+    );
+});
+
+gulp.task('watch-source', ['build']);
+
+gulp.task('serve', function () {
+    gulp.watch(['js/**/*.js', '*.html'], ['watch-source']);
+});
