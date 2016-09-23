4917d3f29c1480bcc219842db27f8652984a78c4
thecyclingfarmer <martgust@gmail.com>
2016-09-23 15:50:35 +0200

    Removed old code

--- app/framework/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import React from 'react';
-import CheckboxWithLabel from '../components/component-checkboxwithlabel/component.checkboxwithlabel.react';
-
-React.render(<CheckboxWithLabel labelOn="On" labelOff="Off" />, document.getElementById('view'));
--- config/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-var del = require('del');
-module.exports = function (gulp, plugins) {
-  return function () {
-    //CLEAN
-    gulp.task('clean', function (cb) {
-      del('js', cb() );
-    });
-  };
-};
